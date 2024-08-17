812b59c47db15445bd73e8c730507a4a7227da67
Aldair828 <aldairdurand6@gmail.com>
2024-08-17 00:02:20 -0500

    Update rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,28 +1,55 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { createHash } from 'crypto' 
 import PhoneNumber from 'awesome-phonenumber'
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix }) => {
+  let fkontak = {
+    "key": {
+      "participants": "0@s.whatsapp.net",
+      "remoteJid": "status@broadcast",
+      "fromMe": false,
+      "id": "Halo"
+    },
+    "message": {
+      "contactMessage": {
+        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+      }
+    },
+    "participant": "0@s.whatsapp.net"
+  }
+
+  let pp = 'https://telegra.ph/file/1f6505497e2ce19a16685.mp4'
+  //const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
+  let user = global.db.data.users[m.sender]
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-  let { exp, limit, name, registered, regTime, age, level } = global.db.data.users[who]
-  let { min, xp, max } = xpRange(user.level, global.multiplier)
-  let username = conn.getName(who)
-  let prem = global.prems.includes(who.split`@`[0])
-  let txt = `╭─⬣「 *User Perfil* 」⬣\n`
-     txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
-     txt += `│  ≡◦ *🐢 Edad ∙* ${age} años\n`
-     
-     txt += `│  ≡◦ *📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
-     txt += `│  ≡◦ *🍬 Dulces ∙* ${limit}\n`
-     txt += `│  ≡◦ *💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
-     txt += `│  ≡◦ *👑 Premium ∙* ${prem ? 'Si' : 'No'}\n`
-     txt += `╰─⬣`
-await conn.sendFile(m.chat, pp, 'thumbnail.jpg', txt, m)
+  try {
+    pp = await conn.getProfilePicture(who)         //pp = await conn.getProfilePicture(who)
+  } catch (e) {
+    // Handle errors here if necessary
+  } finally {
+    let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
+    //let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let mentionedJid = [who]
+    let username = conn.getName(who)
+    let prem = global.prems.includes(who.split`@`[0])
+    let sn = createHash('md5').update(who).digest('hex')
+    let str = `[#Tatsumaki_Bot]
+
+*DATOS GENERALES*
+
+*[🙎‍♂️] ID →* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*[🗒] NOMBRES →* ${name}
+*[⚡] ALIAS →* ${username}
+*[💰] CREDITOS →* ${limit}
+*[📈] ESTADO →* ${registered ? 'Registrado' : 'No Registrado'}
+*[⏱] ANTI-SPAM →* ${user.antispam || 'No Definido'}`
+    
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, fkontak, false, { contextInfo: { mentionedJid }})
+  }
 }
-handler.help = ['perfil', 'perfil @user']
-handler.tags = ['rg']
-handler.command = ['perfil', 'profile']
-handler.register = true
+
+handler.help = ['profile [@user]']
+handler.tags = ['xp']
+handler.command = /^perfil|profile?$/i
 
 export default handler
