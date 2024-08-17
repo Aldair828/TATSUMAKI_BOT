34f24472ada51297ef055afc3a3c582d7d06d5a2
Aldair828 <aldairdurand6@gmail.com>
2024-08-17 11:08:06 -0500

    Update rpg-dulces.js

--- plugins/rpg-dulces.js
+++ plugins/rpg-dulces.js
@@ -3,11 +3,11 @@ let handler = async (m, {conn, usedPrefix}) => {
    if (who == conn.user.jid) return error 
    if (!(who in global.db.data.users)) return conn.reply(m.chat, 'El usuario no se encuentra en mi base de Datos.', m)
    let user = global.db.data.users[who]
-   await m.reply(`${who == m.sender ? `Tienes *${user.limit} 🍬 Dulces* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.limit} 🍬 Dulces* en su Cartera`}. `, null, { mentions: [who] })
+   await m.reply(`${who == m.sender ? `Tienes *${user.limit} 💸 Creditos* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.limit} 🍬 Dulces* en su Cartera`}. `, null, { mentions: [who] })
 }
 
-handler.help = ['dulces']
+handler.help = ['creditos']
 handler.tags = ['rpg']
-handler.command = ['wallet', 'cartera', 'dulces', 'bal', 'coins']
+handler.command = ['wallet', 'cartera', 'creditos', 'bal', 'coins']
 handler.register = true 
 export default handler
