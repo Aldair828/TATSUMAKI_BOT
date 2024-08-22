33d2e25aec0d66a58513705188af9e48bfe1ef2a
Aldair828 <aldairdurand6@gmail.com>
2024-08-22 06:34:38 -0500

    Update img-megumin.js

--- plugins/img-megumin.js
+++ plugins/img-megumin.js
@@ -7,9 +7,14 @@ let handler = async (m, { conn, isOwner }) => {
 
     users.forEach(([jid, user]) => {
         user.limit = 20; // Restablece los créditos a 20
+        user.bank = 0;   // Reinicia los créditos en el banco a 0
+        // Elimina otros posibles atributos relacionados con créditos
+        if (user.extraCredits) user.extraCredits = 0; // Si existe un campo extra de créditos
+        if (user.profileCredits) user.profileCredits = 0; // Si existe un campo de créditos en el perfil
+        // Añadir aquí cualquier otro campo relevante que deba reiniciarse
     });
 
-    conn.reply(m.chat, 'Se han reiniciado todos los créditos a 20 para todos los usuarios.', m);
+    conn.reply(m.chat, 'Se han reiniciado todos los créditos a 20 y se han eliminado todos los créditos adicionales para todos los usuarios.', m);
 }
 
 handler.help = ['topcero'];
