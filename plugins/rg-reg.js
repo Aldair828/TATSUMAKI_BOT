6681fa6fb6b6a5db640aecbc44eefc832eab7381
Aldair828 <aldairdurand6@gmail.com>
2024-08-25 16:08:26 -0500

    Update rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -7,45 +7,35 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) {
-    await conn.sendButton(m.chat, 
-      `🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`, 
-      `Presiona el botón de abajo para ver el menú.`,
-      'Menú', 
-      `${usedPrefix}menu`,
-      m)
-    return
-  }
-  
-  if (!Reg.test(text)) return m.reply(`🍭 Formato incorrecto.\n\nUso del comando: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
-  
+  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`🍭 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) return m.reply('🍭 El nombre no puede estar vacío.')
   if (!age) return m.reply('🍭 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('🍭 El nombre es demasiado largo.')
-  
+  if (name.length >= 100) return m.reply('🍭 El nombre es demasiado largo.' )
   age = parseInt(age)
   if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
-  if (age < 5) return m.reply('🚼 hay un abuelo bebé jsjsj.')
-  
+  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
   user.name = name.trim()
   user.age = age
-  user.regTime = +new Date
+  user.regTime = + new Date
   user.registered = true
-  
   let sn = createHash('md5').update(m.sender).digest('hex')
   let img = await (await fetch(`https://tinyurl.com/2cpfjwp4`)).buffer()
   let txt = '`– R E G I S T R O  -  U S E R`\n\n'
       txt += `┌  ✩  *Nombre* : ${name}\n`
       txt += `│  ✩  *Edad* : ${age} años\n`
-      txt += `│  ✩  *Número de serie*\n`
-      txt += `└  ✩  ${sn}\n\n`
-      txt += `*𝙿𝙰𝚁𝙰 𝚅𝙸𝚂𝚄𝙰𝙻𝙸𝚉𝙰𝚁 𝚃𝚄 𝙿𝙴𝚁𝙵𝙸𝙻 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾* .perfil\n`
-      txt += `*𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚄𝚂𝙰* .menu\n`
-      txt += `*𝙿𝙰𝚁𝙰 𝙲𝚄𝙰𝙻𝚀𝚄𝙸𝙴𝚁 𝙳𝚄𝙳𝙰 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾* .owner`
-  
-  await conn.sendButton(m.chat, txt, '', 'Menú', `${usedPrefix}menu`, m, { image: img })
-  await m.react('✅')
+      txt += `│  ✩  *Numero de serie*\n`
+      txt += `└  ✩  ${sn}
+    
+      *𝙿𝙰𝚁𝙰 𝚅𝙸𝚂𝚄𝙰𝙻𝙸𝚉𝙰𝚁 𝚃𝚄 𝙿𝙴𝚁𝙵𝙸𝙻 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*
+      .perfil
+      *𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚄𝚂𝙰*
+      .menu 
+      *𝙿𝙰𝚁𝙰 𝙲𝚄𝙰𝙻𝚀𝚄𝙸𝙴𝚁 𝙳𝚄𝙳𝙰 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*
+      .owner `
+await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
+await m.react('✅')
 }
 handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
 handler.tags = ['rg']
