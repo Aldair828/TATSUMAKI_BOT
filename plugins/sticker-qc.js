46291c4f5298204a969e09d43cf0389fa997a9f7
Aldair828 <aldairdurand6@gmail.com>
2024-08-26 21:46:16 -0500

    Update sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,39 +1,31 @@
 let handler = async (m, { conn }) => {
-    let users = Object.entries(global.db.data.users)
-        .map(([key, value]) => ({
-            id: key,
-            xp: value.xp,
-            nivel: calcularNivel(value.xp),
-        }))
-        .filter(user => user.xp > 0)
-        .sort((a, b) => b.xp - a.xp)
-        .slice(0, 29); // Mostrar los primeros 29 usuarios
-
-    let texto = '*🏆 TOP XP 🏆*\n\n';
-
-    users.forEach((user, index) => {
-        texto += `${index + 1})\n*[👤] USUARIO:* https://wa.me/${user.id.split('@')[0]}\n*[💫] NIVEL:* ${user.nivel}\n\n`;
+    let users = Object.entries(global.db.data.users).map(([key, value]) => {
+        return { ...value, id: key };
     });
 
-    await conn.reply(m.chat, texto.trim(), m);
-}
+    users.sort((a, b) => b.xp - a.xp);
+
+    let niveles = [150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 76800];
+
+    let top = users.slice(0, 29).map((user, index) => {
+        let nivel = 0;
+        for (let i = 0; i < niveles.length; i++) {
+            if (user.xp >= niveles[i]) {
+                nivel = i + 1;
+            } else {
+                break;
+            }
+        }
+        return `\n${index + 1})\n*[💮] NOMBRE:* ${user.name || "Usuario desconocido"}\n*[👤] USUARIO:* https://wa.me/${user.id.split('@')[0]}\n*[💫] NIVEL:* ${nivel}`;
+    }).join('\n');
+
+    let mensaje = `🏆 *TOP XP* 🏆\n${top}`;
+
+    await conn.reply(m.chat, mensaje, m);
+};
 
 handler.help = ['topxp'];
 handler.tags = ['xp'];
-handler.command = ['topxp'];
+handler.command = ['topxp', 'top'];
 
 export default handler;
-
-// Función para calcular el nivel basado en la XP
-function calcularNivel(xp) {
-    const niveles = [150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 76800];
-    let nivel = 0;
-    for (let i = 0; i < niveles.length; i++) {
-        if (xp >= niveles[i]) {
-            nivel = i + 1;
-        } else {
-            break;
-        }
-    }
-    return nivel;
-}
