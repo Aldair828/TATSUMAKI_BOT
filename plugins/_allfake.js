7916223a405a22a63e27f4c833cc803c21c52fab
Aldair828 <aldairdurand6@gmail.com>
2024-08-27 10:20:22 -0500

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -9,7 +9,7 @@ let img = catalogo
     forwardedNewsletterMessageInfo: {
       newsletterJid: "120363283344753953@newsletter",
       serverMessageId: 100,
-      newsletterName: 'ＴＡＴＳＵＭＡＫＩ___ＢＯＴ',
+      newsletterName: 'ＦＥＮＩＸ___ＢＯＴ',
     },
 	    externalAdReply: {
 				    showAdAttribution: true,
@@ -18,7 +18,7 @@ let img = catalogo
 					mediaUrl: null,
 					description: null,
 					previewType: "PHOTO",
-					thumbnailUrl: 'https://telegra.ph/file/ab013f77c6620ab82c8c9.jpg',
+					thumbnailUrl: 'https://telegra.ph/file/2533dca18fd7459d151cf.jpg',
 		           sourceUrl: canal,
 		           mediaType: 1,
                    renderLargerThumbnail: false
