2d1ad4e8d29f0328f343b30cf0f2700119096b91
Aldair828 <aldairdurand6@gmail.com>
2024-08-26 21:48:32 -0500

    Update sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,31 +1,30 @@
 let handler = async (m, { conn }) => {
-    let users = Object.entries(global.db.data.users).map(([key, value]) => {
-        return { ...value, id: key };
-    });
+    let users = Object.entries(global.db.data.users)
+        .map(([key, value]) => ({ ...value, jid: key }))
+        .filter(user => user.xp > 0)
+        .sort((a, b) => b.xp - a.xp)
+        .slice(0, 29);
 
-    users.sort((a, b) => b.xp - a.xp);
-
-    let niveles = [150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 76800];
-
-    let top = users.slice(0, 29).map((user, index) => {
+    let text = users.map((user, i) => {
+        // Calcular el nivel según el XP
+        const niveles = [150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 76800];
         let nivel = 0;
-        for (let i = 0; i < niveles.length; i++) {
-            if (user.xp >= niveles[i]) {
-                nivel = i + 1;
+        for (let j = 0; j < niveles.length; j++) {
+            if (user.xp >= niveles[j]) {
+                nivel = j + 1;
             } else {
                 break;
             }
         }
-        return `\n${index + 1})\n*[💮] NOMBRE:* ${user.name || "Usuario desconocido"}\n*[👤] USUARIO:* https://wa.me/${user.id.split('@')[0]}\n*[💫] NIVEL:* ${nivel}`;
+
+        return `${i + 1})\n*[💮] NOMBRE:* ${conn.getName(user.jid)}\n*[👤] USUARIO:* https://wa.me/${user.jid.split('@')[0]}\n*[⭐] XP:* ${user.xp}\n*[💫] NIVEL:* ${nivel}\n`;
     }).join('\n');
 
-    let mensaje = `🏆 *TOP XP* 🏆\n${top}`;
-
-    await conn.reply(m.chat, mensaje, m);
+    conn.reply(m.chat, text, m);
 };
 
 handler.help = ['topxp'];
 handler.tags = ['xp'];
-handler.command = ['topxp', 'top'];
+handler.command = ['top'];
 
 export default handler;
