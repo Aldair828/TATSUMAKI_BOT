e28cad7c791374a6c6304c757fd728f5472ed795
Aldair828 <aldairdurand6@gmail.com>
2024-08-29 01:05:57 -0500

    Update downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -1,11 +1,10 @@
-// Comando .topcreditos
 let handler = async (m, { conn }) => {
     let users = Object.entries(global.db.data.users)
         .filter(([jid, user]) => user.registered)
         .sort(([, a], [, b]) => b.limit - a.limit)
-        .slice(0, 29); // Top 50 usuarios
+        .slice(0, 29); // Top 29 usuarios
 
-    let str = 'PUEDES PEDIR UN PRESTAMO CON EL COMANDO  .prestamo\n\nSi usted tiene dinero en el banco y no lo ha retirado, no va a salir en el top porque son creditos guardados\n\n▂▃▄▅▆▇█▓▒░ 𝐓𝐎𝐏 👑 ░▒▓█▇▆▅▄▃▂\n\n';
+    let str = 'PUEDES PEDIR UN PRÉSTAMO CON EL COMANDO .prestamo\n\nSi usted tiene dinero en el banco y no lo ha retirado, no va a salir en el top porque son créditos guardados\n\n▂▃▄▅▆▇█▓▒░ 𝐓𝐎𝐏 👑 ░▒▓█▇▆▅▄▃▂\n\n';
 
     users.forEach(([jid, user], index) => {
         let rank;
@@ -19,7 +18,8 @@ let handler = async (m, { conn }) => {
         str += `${index + 1})\n*[👤] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${conn.getName(jid)}\n*[📱] 𝙽𝚄𝙼𝙴𝚁𝙾:* https://wa.me/${jid.split('@')[0]}\n*[💸] 𝙲𝚁𝙴́𝙳𝙸𝚃𝙾𝚂:* ${user.limit}\n*[🔱] 𝚁𝙰𝙽𝙶𝙾:* ${rank}\n\n`;
     });
 
-    conn.reply(m.chat, str.trim(), m);
+    let imageUrl = 'https://telegra.ph/file/13e6e5f9038ad6cc9b3f1.jpg'; // Reemplaza este enlace con el de tu imagen
+    conn.sendFile(m.chat, imageUrl, 'topcreditos.jpg', str.trim(), m);
 }
 
 handler.help = ['top'];
