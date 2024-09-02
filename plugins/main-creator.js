84cba5b5666d7023acd63b0d13b35c060558963c
Aldair828 <aldairdurand6@gmail.com>
2024-09-02 00:04:13 -0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, isOwner }) => {
 
     let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:Kevib 🤖;;\nFN:Abraham PRO YT 🇲🇽\nORG:𝚂𝙾𝙿𝙾𝚁𝚃𝙴 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 🌐\nTITLE:\nitem1.TEL;waid=522482267952:+522482267952\nitem1.X-ABLabel:Abraham PRO YT 🇲🇽\nX-WA-BIZ-DESCRIPTION:Abraham PRO YT 🇲🇽\nX-WA-BIZ-NAME:Abraham PRO YT 🇲🇽\nEND:VCARD`;
 
-    let vcard3 = `BEGIN:VCARD\nVERSION:3.0\nN:Kevin 🤖;;\nFN:Kevin\nORG:𝚂𝙾𝙿𝙾𝚁𝚃𝙴 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 🌐\nTITLE:\nitem1.TEL;waid=5493624187763:+5493624187763\nitem1.X-ABLabel:KEVIN\nX-WA-BIZ-DESCRIPTION:KEVIN\nX-WA-BIZ-NAME:KEVIN\nEND:VCARD`;
+    let vcard3 = `BEGIN:VCARD\nVERSION:3.0\nN:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻;;\nFN:Kevin\nORG:𝚂𝙾𝙿𝙾𝚁𝚃𝙴 𝙵𝙽𝚇 𝙱𝙾𝚃\nTITLE:\nitem1.TEL;waid=5493624187763:+5493624187763\nitem1.X-ABLabel:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nX-WA-BIZ-DESCRIPTION:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nX-WA-BIZ-NAME:𝑲𝑬𝑽𝑰𝑵 𝑺𝑶𝑷𝑶𝑹𝑻\nEND:VCARD`;
 
     let vcard4 = `BEGIN:VCARD\nVERSION:3.0\nN:Ajolote 🤖;;\nFN:Jorge\nORG:𝚂𝚄𝙱𝙾𝚃 🌐\nTITLE:\nitem1.TEL;waid=51962506486:+51962506486\nitem1.X-ABLabel:JORGE\nX-WA-BIZ-DESCRIPTION:JORGE\nX-WA-BIZ-NAME:JORGE\nEND:VCARD`;
 
