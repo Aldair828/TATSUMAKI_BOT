5d577dbf1a20ffb3fa0cd2241cab97685e5104dd
Aldair828 <aldairdurand6@gmail.com>
2024-09-02 21:30:10 -0500

    Create main-menu1.js

---
+++ plugins/main-menu1.js
@@ -0,0 +1,101 @@
+import { promises } from 'fs'
+import { join } from 'path'
+
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+  try {
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, limit, level } = global.db.data.users[m.sender]
+    let name = await conn.getName(m.sender)
+    let d = new Date(new Date() + 3600000)
+    let locale = 'es'
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+
+    // Determinar si hay códigos disponibles
+    let availableCodes = global.db.data.codes && Object.keys(global.db.data.codes).length > 0;
+
+    // Texto del nuevo menú
+    let menuText = `
+
+*Bienvenido* @${name} 
+
+╭──────༺♡༻──────╮
+                *INFO-BOT*
+╰──────༺♡༻──────╯
+
+
+➢ .owner 
+➥ ve los contactos de los creadores 
+
+➢ .grupos 
+➥ ve los grupos y canales oficiales del bot 
+
+➢ .estado 
+➥ ve el estado del bot 
+
+➢ .totalfunciones 
+➥ ve cuantas funciones tiene el bot 
+
+➢ .ping 
+➥ ve la velocidad del bot 
+
+➢ .runtime 
+➥ ve cuanto tiempo lleva activo el bot
+
+
+
+.menu   para volver a la lista de menu 
+`.trim()
+
+    let imageUrl = 'https://telegra.ph/file/52034ef36501ddbadfd0b.jpg' // Reemplaza esto con el enlace directo a tu imagen
+    await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: menuText }, { quoted: m })
+
+  } catch (e) {
+    conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
+    throw e
+  }
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['menu', 'help', 'menú'] 
+handler.register = true 
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
