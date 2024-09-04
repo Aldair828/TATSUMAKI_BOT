52b8a5a8b709361b4d833fbb7250754644dc990c
Aldair828 <aldairdurand6@gmail.com>
2024-09-04 00:26:58 -0500

    Update main-chatgpt.js

--- plugins/main-chatgpt.js
+++ plugins/main-chatgpt.js
@@ -1,53 +1,64 @@
 import fetch from 'node-fetch';
 import axios from 'axios';
 import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
+import { Configuration, OpenAIApi } from 'openai';
 
-if (command == 'ia' || command == 'chatgpt') {
-try {     
-await conn.sendPresenceUpdate('composing', m.chat)
-
-async function luminsesi(q, username, logic) {
-try {
-const response = await axios.post("https://lumin-ai.xyz/", {
-content: q,
-user: username,
-prompt: logic,
-webSearchMode: true // true = resultado con url
+const configuration = new Configuration({
+  organization: global.openai_org_id,
+  apiKey: global.openai_key,
 });
-return response.data.result;
-} catch (error) {
-console.error('Error al obtener:', error);
-}}
 
-let query = m.text;
-let username = `${m.pushName}`;
+const openaiii = new OpenAIApi(configuration);
 
-let syms1 = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD 🐈`;  
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    return m.reply('𝔼𝕊ℂℝ𝕀𝔹𝔸 𝕊𝕌 ℙℝ𝔼𝔾𝕌ℕ𝕋𝔸 ℙ𝔸ℝ𝔸 ℚ𝕌𝔼 𝔽𝔼́ℕ𝕀𝕏 𝕃𝔼 ℝ𝔼𝕊ℙ𝕆ℕ𝔻𝔸');
+  }
 
-let result = await luminsesi(query, username, syms1);
- await m.reply(result)
-} catch {
-try {
-let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
-let res = await gpt.json()
-await m.reply(res.gpt)
-/*let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/chatgpt?q=${text}`)
-let res = await gpt.json()
-await m.reply(res.data)*/
-} catch {
-}}}
+  if (command === 'fenixgpt') {
+    try {
+      await conn.sendPresenceUpdate('composing', m.chat);
 
-if (command == 'openai' || command == 'ia2' || command == 'chatgpt2') {
-conn.sendPresenceUpdate('composing', m.chat);
-let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
-let res = await gpt.json()
-await m.reply(res.gpt)
-}}
-handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
+      async function luminsesi(q, username, logic) {
+        try {
+          const response = await axios.post("https://lumin-ai.xyz/", {
+            content: q,
+            user: username,
+            prompt: logic,
+            webSearchMode: true // true = resultado con url
+          });
+          return response.data.result;
+        } catch (error) {
+          console.error('Error al obtener:', error);
+        }
+      }
+
+      let query = m.text;
+      let username = `${m.pushName}`;
+
+      let syms1 = `Actuarás como un Bot de WhatsApp el cual fue creado por Aldair, tu serás Fenix_bot 🐦‍🔥`;
+
+      let result = await luminsesi(query, username, syms1);
+      await m.reply(result);
+    } catch {
+      try {
+        let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`);
+        let res = await gpt.json();
+        await m.reply(res.gpt);
+      } catch {
+        // Manejar cualquier error adicional si es necesario
+      }
+    }
+  }
+
+  if (command === 'fenixgpt2') {
+    conn.sendPresenceUpdate('composing', m.chat);
+    let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`);
+    let res = await gpt.json();
+    await m.reply(res.gpt);
+  }
+};
+
+handler.command = /^(fenixgpt|fenixgpt2)$/i;
+
 export default handler;
